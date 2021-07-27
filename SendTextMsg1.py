5c538715f9e95971d1abf6446825fa90fa36469f
x86osx <65531786+x86osx@users.noreply.github.com>
2021-07-28 08:22:37 +0900

    텍스트 메시지와 리스트 메시지 전송

---
+++ SendTextMsg1.py
@@ -0,0 +1,37 @@
+import kakao_utils
+
+
+KAKAO_TOKEN_FILENAME = "res/kakao_message/kakao_token.json"
+KAKAO_APP_KEY = "6148d6e7b45b3f0b0cc0b38f6739a45b"
+
+# 저장된 토큰 정보를 읽어 옴
+tokens = kakao_utils.update_tokens(KAKAO_APP_KEY, KAKAO_TOKEN_FILENAME)
+# 업데이트한 토큰 저장하기
+#save_tokens(KAKAO_TOKEN_FILENAME, tokens)
+# 텍스트 메시지 url
+url = "https://kapi.kakao.com/v2/api/talk/memo/default/send"
+
+# request parameter 설정
+headers = {
+    "Authorization": "Bearer " + tokens['access_token']
+}
+
+
+data = {
+    "template_object" : json.dumps({ "object_type" : "text",
+                                     "text" : "Hello, world!",
+                                     "link" : {
+                                         "web_url" : "www.naver.com"
+                                     }
+                                     })
+}
+
+# 나에게 카카오톡 메시지 보내기 요청 (text)
+response = requests.post(url, headers=headers, data=data)
+print(response.status_code)
+
+# 요청에 실패했다면,
+if response.status_code != 200:
+    print("error! because ", response.json())
+else: # 성공했다면,
+    print('메시지를 성공적으로 보냈습니다.')
