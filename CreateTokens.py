2b707cc6c816705a70007f041a29748a903cb103
x86osx <65531786+x86osx@users.noreply.github.com>
2021-07-28 08:27:18 +0900

    Update CreateTokens.py

--- CreateTokens.py
+++ CreateTokens.py
@@ -7,9 +7,9 @@ url="https://kauth.kakao.com/oauth/token"
 
 data = {
     "grant_type" : "authorization_code",
-    "client_id" : "6148d6e7b45b3f0b0cc0b38f6739a45b",
+    "client_id" : "My REST Key",
     "redirect_uri" : "https://localhost.com",
-    "code" : "bhUNmd6TCMuk8Z8vYejWydbGvKmQ54si7yEjvl2O3yDcLzKKhqk3E4TjcMExJYfXT7Wo7Ao9dJcAAAF66RQ9UA"
+    "code" : "My Code"
 }
 
 response = requests.post(url, data=data)
@@ -22,7 +22,3 @@ else :
     with open("kakao_token.json", "w") as fp:
         json.dump(tokens, fp)
 
-'''
-{'access_token': '07ARBNfH9hTSeoE6y3WeaNGfX2WuzmEsRxKPAAopcFEAAAF66Obt2w', 'token_type': 'bearer', 'refresh_token': 'r-Tj9KWLlPHqAPI3NBzVm3nnPwEiHy_9Mf1qYAopcFEAAAF66Obt2Q', 'expires_in': 21599, 'scope': 'account_email profile_image talk_message profile_nickname', 'refresh_token_expires_in': 5183999}
-
-'''
